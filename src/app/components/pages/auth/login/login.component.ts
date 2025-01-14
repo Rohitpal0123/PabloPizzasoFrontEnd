d1ab13bfb2fa4253d658d7d7bca26a43a34fc93b
Rohitpal0123 <prohit9370@gmail.com>
2025-01-14 17:42:06 +0530

    added jwt auth in login

--- src/app/components/pages/auth/login/login.component.ts
+++ src/app/components/pages/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../../services/auth.service';
 import { Login } from '../../../../models/Login';
 import { LoginFormComponent } from '../../../shared/login-form/login-form.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,17 @@ import { LoginFormComponent } from '../../../shared/login-form/login-form.compon
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onLoginSubmit(credentials: Login) {
     const loginData: Login = credentials;
 
+
+
     this.authService.login(loginData).subscribe({
       next: (response) => {
         console.log('Login successful', response);
+        this.router.navigate(['/home']);
       },
       error: (error) => {
         console.log('Login failed', error);
